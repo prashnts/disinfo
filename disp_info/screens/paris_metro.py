bd4c8c5e0c7084ac0bb098f066342f6817a64d40
Prashant Sinha <prashant@noop.pw>
2023-05-07 13:01:49 +0200

    adjust color and pixels

--- disp_info/screens/paris_metro.py
+++ disp_info/screens/paris_metro.py
@@ -10,22 +10,24 @@ from disp_info.components.layouts import composite_at, stack_horizontal
 metro_colors = {
     '1': ['#FFCE00', '#000'],
     '4': ['#C04191', '#fff'],
-    '6': ['#83C491', '#000'],
-    '8': ['#CEADD2', '#000'],
-    '13': ['#98D4E2', '#000'],
+    '6': ['#50a863', '#000'],
+    '8': ['#b171b8', '#000'],
+    '13': ['#57abbe', '#000'],
 }
 
 
 @cache
 def metro_icon(line_name: str) -> Frame:
-    size = 10
+    size = 9
     background, text_color = metro_colors.get(line_name, ['#C6C6C6', '#000'])
 
-    img = Image.new('RGBA', (size, size))
+    img = Image.new('RGBA', (size + 1, size + 1))
     draw = ImageDraw.Draw(img)
 
-    draw.ellipse([0, 0, size - 1, size - 1], fill=background)
-    draw.text(((size - 1) / 2, (size - 1)/2), line_name, fill=text_color, font=fonts.tamzen__rs, anchor='mm')
+    start_x = 0 if len(line_name) > 1 else 1
+
+    draw.ellipse([0, 0, size, size], fill=background)
+    draw.text(((size / 2) + start_x, size / 2), line_name, fill=text_color, font=fonts.tamzen__rs, anchor='mm')
 
     return Frame(img)
 
