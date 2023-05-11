9eafc1a05d0c32d3092333445a58d1099a20ffba
Prashant Sinha <prashant@noop.pw>
2023-05-11 13:19:26 +0200

    throttle pir sensor

--- disp_info/state_proxy.py
+++ disp_info/state_proxy.py
@@ -1,8 +1,11 @@
 import arrow
 
+from pydash import py_
+
 from .redis import get_dict, rkeys
 
-def should_turn_on_display() -> bool:
+
+def _should_turn_on_display() -> bool:
     def is_motion_detected(name: str) -> bool:
         pir_state = get_dict(rkeys[name])
         if not pir_state:
@@ -29,3 +32,5 @@ def should_turn_on_display() -> bool:
 
     # if any sensor reports True we keep the display on.
     return any(motion_states)
+
+should_turn_on_display = py_.throttle(_should_turn_on_display, 500)
