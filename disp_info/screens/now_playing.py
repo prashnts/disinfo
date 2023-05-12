5a18d2179abdc0752ee4deff36d218f42f4e4b71
Prashant Sinha <prashant@noop.pw>
2023-05-12 21:39:10 +0200

    better music

--- disp_info/screens/now_playing.py
+++ disp_info/screens/now_playing.py
@@ -1,32 +1,65 @@
 import requests
 import io
+import arrow
 
 from PIL import Image
 from functools import cache
+from datetime import timedelta
 
 from disp_info import config
 from disp_info.components import fonts
 from disp_info.components.elements import Frame
 from disp_info.components.text import Text
-from disp_info.components.layouts import stack_horizontal, stack_vertical
+from disp_info.components.layouts import stack_horizontal, stack_vertical, composite_at
 from disp_info.sprite_icons import SpriteImage
 from disp_info.redis import rkeys, get_dict
 from disp_info.components.scroller import HScroller
 from disp_info.utils import throttle
 
-hscroller = HScroller(size=30, delta=1, speed=0.01, pause_at_loop=True, pause_duration=1)
-play_icon = SpriteImage('assets/raster/play.9x9.png')[0]
+hscroller = HScroller(size=20, delta=1, speed=0.01, pause_at_loop=True, pause_duration=1)
+
+play_icon = SpriteImage('assets/raster/play-5x5.png')[0]
+pause_icon = SpriteImage('assets/raster/pause-5x5.png')[0]
+spotify_icon = SpriteImage('assets/raster/spotify-5x5.png')[0]
 
 text_music_info = Text(font=fonts.bitocra, fill='#a1a9b0')
 
 
 @throttle(1033)
 def get_state():
-    return get_dict(rkeys['ha_sonos_beam'])
+    state = dict(is_visible=False)
+
+    s = get_dict(rkeys['ha_sonos_beam']).get('new_state')
+
+    if not s:
+        return state
+
+    state['playing'] = s['state'] == 'playing'
+    state['paused'] = s['state'] == 'paused'
+
+    last_change = arrow.get(s['last_changed']).to('local')
+    now = arrow.now()
+
+    state['media_title'] = s['attributes'].get('media_title')
+    state['media_album'] = s['attributes'].get('media_album_name')
+    state['media_artist'] = s['attributes'].get('media_artist')
+    state['is_spotify'] = 'Spotify' in s['attributes']['source']
+    state['album_art'] = get_album_art(
+        s['attributes'].get('entity_picture'),
+        media_album=state['media_album'],
+        is_spotify=state['is_spotify'])
+
+    state['is_visible'] = all([
+        state['playing'] or state['paused'],
+        state['media_title'] != 'TV',
+        (last_change + timedelta(minutes=30)) > now,
+    ])
+
+    return state
 
 @cache
-def get_album_art(fragment: str, media_title: str):
-    if 'franceinfo' in media_title:
+def get_album_art(fragment: str, media_album: str, is_spotify: bool=False):
+    if 'franceinfo' in media_album:
         # Hard code some album arts.
         return SpriteImage('assets/raster/france-info.png')[0]
     try:
@@ -35,35 +68,26 @@ def get_album_art(fragment: str, media_title: str):
         fp = io.BytesIO(r.content)
         img = Image.open(fp)
         # Dithering helps
-        img = img.resize((80, 80)).quantize()
-        return Frame(img.resize((25, 25)).convert('RGBA'))
+        img = img.resize((80, 80)).quantize().resize((25, 25)).convert('RGBA')
+        if is_spotify:
+            img = composite_at(spotify_icon, img, 'bl')
+        return Frame(img)
     except requests.RequestException:
         return None
 
 def draw():
-    state = get_state()
+    s = get_state()
 
-    if not state.get('new_state'):
+    if not s['is_visible']:
         return
 
-    state = state['new_state']
-    media_info = ''
-
-    if state['state'] == 'playing':
-        media_title = state['attributes'].get('media_title')
-        media_album = state['attributes'].get('media_album_name')
-        media_artist = state['attributes'].get('media_artist')
-
-        elements = [media_title, media_album, media_artist]
-        media_info = ' >> '.join([e for e in elements if e])
-    else:
-        return
-
-    if not media_info or media_title == 'TV':
-        return
+    elements = [s['media_title'], s['media_album'], s['media_artist']]
+    media_info = ' >> '.join([e for e in elements if e])
 
 
-    art = get_album_art(state['attributes'].get('entity_picture'), media_title)
+    art = s['album_art']
+
+    act_icon = play_icon if s['playing'] else pause_icon
 
     changed = text_music_info.update(value=media_info)
     # "Refresh" the scroller with the change status of the frame.
@@ -71,7 +95,7 @@ def draw():
 
     music_elements = [
         stack_horizontal([
-            play_icon,
+            act_icon,
             hscroller.draw(),
         ], gap=0)
     ]
