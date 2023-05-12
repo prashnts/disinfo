0e5fa6f8f7b28c1d1d6dae0a40794e88995c0453
Prashant Sinha <prashant@noop.pw>
2023-05-12 22:55:16 +0200

    misc

--- disp_info/screens/now_playing.py
+++ disp_info/screens/now_playing.py
@@ -110,6 +110,6 @@ def draw():
 
     return add_background(
         stack_vertical(music_elements, gap=1, align='right'),
-        fill='#4a5462',
+        fill='#333941',
         radius=2,
         padding=2)
