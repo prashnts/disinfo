ff3eab5a07c741cc3de6eff38fc1b90919625533
Prashant Sinha <prashant@noop.pw>
2023-05-14 20:55:16 +0200

    limit fps

--- disp_info/matrix_renderer.py
+++ disp_info/matrix_renderer.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import typer
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
@@ -23,13 +25,34 @@ options.hardware_mapping = 'regular'
 matrix = RGBMatrix(options = options)
 double_buffer = matrix.CreateFrameCanvas()
 
-try:
-    print("Press CTRL-C to stop.")
+
+def main(fps: int = 60, stats: bool = False):
+    print('Matrix Renderer started')
+    _tf = 1 / fps
 
     while True:
+        t_a = time.time()
         img = get_frame()
+        t_b = time.time()
         double_buffer.SetImage(img.convert('RGB'))
         double_buffer = matrix.SwapOnVSync(double_buffer)
-        # time.sleep(0.005)
-except KeyboardInterrupt:
-    sys.exit(0)
+        t_c = time.time()
+
+        t_draw = t_b - t_a
+        t_matrix = t_c - t_b
+        t_frame = t_c - t_a
+
+        delay = max(_tf - t_frame, 0)
+        _fps = (1 / (t_frame + delay))
+
+        if stats:
+            print('\033[2J')
+            print(f't draw:      {t_draw:0.4}')
+            print(f't sixel:     {t_matrix:0.4}')
+            print(f'frame delay: {delay}')
+            print(f'fps:         \033[34m{_fps:0.4}\033[0m')
+
+        time.sleep(delay)
+
+if __name__=='__main__':
+    typer.run(main)
--- disp_info/sixel_renderer.py
+++ disp_info/sixel_renderer.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageEnhance, ImageDraw
 
 from .renderer import get_frame
 
-def draw_panel(img, scale=4, gap=1):
+def enlarge_pixels(img, scale=4, gap=1):
     # turn the img into a mosaic with gap between px.
     outline_color = '#000000'
     radius = 2
@@ -31,31 +31,65 @@ def draw_panel(img, scale=4, gap=1):
     enhancer = ImageEnhance.Brightness(i)
     return enhancer.enhance(1.5)
 
-def main(single_frame: bool = False):
+def encode_sixels(img: Image, optimize: bool = False, scale=4, gap=1) -> str:
+    '''Encodes given image to a sixel string.'''
+    if optimize:
+        img = enlarge_pixels(img, scale, gap)
+
+    buf = BytesIO()
+    data = img.convert('RGB').tobytes()
+    width = img.width
+    height = img.height
+    output = sixel_output_new(lambda data, s: s.write(data), buf)
+    dither = sixel_dither_new(256)
+    sixel_dither_initialize(dither, data, width, height, SIXEL_PIXELFORMAT_RGB888)
+    sixel_encode(data, width, height, 1, dither, output)
+
+    sixel = buf.getvalue().decode('ascii')
+
+    sixel_dither_unref(dither)
+    sixel_output_unref(output)
+    buf.close()
+
+    return sixel
+
+
+def main(single_frame: bool = False, fps: int = 60, scale: int = 4):
     if not single_frame:
         # First we clear the screen.
         print('\033[2J')
 
+    _tf = 1 / fps
+
     while True:
-        img = draw_panel(get_frame())
-        buf = BytesIO()
-        data = img.convert('RGB').tobytes()
-        width = img.width
-        height = img.height
-        output = sixel_output_new(lambda data, s: s.write(data), buf)
-        dither = sixel_dither_new(256)
-        sixel_dither_initialize(dither, data, width, height, SIXEL_PIXELFORMAT_RGB888)
-        sixel_encode(data, width, height, 1, dither, output)
+        t_a = time.time()
+        frame = get_frame()
+        t_b = time.time()
+        fsixel = encode_sixels(frame, optimize=True, scale=scale)
 
         # Term Cursor Position x, y ; ref colorama
         print('\033[0;0H')
 
         # Print sixel at previous location.
-        print(buf.getvalue().decode('ascii'))
+        print(fsixel)
+        t_c = time.time()
 
-        sixel_dither_unref(dither)
-        sixel_output_unref(output)
-        buf.close()
+        # Show various times to execute.
+
+        t_draw = t_b - t_a
+        t_sixel = t_c - t_b
+        t_frame = t_c - t_a
+
+        delay = max(_tf - t_frame, 0)
+        _fps = (1 / (t_frame + delay))
+
+        print(f't draw:      {t_draw:0.4}')
+        print(f't sixel:     {t_sixel:0.4}')
+        print(f'frame delay: {delay}')
+        print(f'fps:         \033[34m{_fps:0.4}\033[0m')
+
+        # Limit frame rate
+        time.sleep(delay)
 
         if single_frame:
             break
