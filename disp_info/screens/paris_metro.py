a9cbdb3071e9a1c63ce1f42bd44c29d34d8c3c16
Prashant Sinha <prashant@noop.pw>
2023-05-23 23:42:12 +0200

    metro deserves a darker bg

--- disp_info/screens/paris_metro.py
+++ disp_info/screens/paris_metro.py
@@ -111,4 +111,4 @@ def draw(tick: float):
     if not train_times:
         return
 
-    return add_background(stack_vertical(train_times, gap=1, align='left'), '#000000ac', padding=2)
+    return add_background(stack_vertical(train_times, gap=1, align='left'), '#000000c9', padding=2)
