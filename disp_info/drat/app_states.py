8f3eeafec6ac45c998067bc1b50aeae801b37956
Prashant Sinha <prashant@noop.pw>
2023-06-03 19:17:16 +0200

    better logic

--- disp_info/drat/app_states.py
+++ disp_info/drat/app_states.py
@@ -1,10 +1,28 @@
 import pendulum
 
+from dataclasses import dataclass
+from typing import Optional, Union
+
 from ..redis import set_dict, get_dict
 from ..data_structures import FrameState
 from .data_service import get_metro_info
 from . import idfm
 
+
+@dataclass
+class MetroAppState:
+    show: bool = False
+    toggled_at: Optional[pendulum.DateTime] = None
+
+
+def in_future(dt: Union[str, pendulum.DateTime, None], seconds: int = 0, minutes: int = 0):
+    if not dt:
+        return False
+
+    if isinstance(dt, str):
+        dt = pendulum.parse(dt)
+
+    return dt.add(seconds=seconds, minutes=minutes) > pendulum.now()
 
 class MetroInfoState:
     name = 'di.state.metroinfo'
@@ -18,8 +36,13 @@ class MetroInfoState:
         if topic in ['zigbee2mqtt/enki.rmt.0x03', 'zigbee2mqtt/ikea.rmt.0x01']:
             if msg['action'] in ['scene_2', 'toggle']:
                 visible = self.value['show']
-                self.set_state(show=not visible, toggle_time=pendulum.now().isoformat())
-                if not visible:
+                if in_future(self.value.get('toggle_time'), seconds=25):
+                    visible = not visible
+                else:
+                    visible = True
+
+                self.set_state(show=visible, toggle_time=pendulum.now().isoformat())
+                if visible:
                     # it was previously not visible, so we refresh.
                     get_metro_info(force=True)
 
@@ -31,8 +54,8 @@ class MetroInfoState:
         if refresh:
             self.refresh()
         state = self.value
-        toggle_time = state.get('toggle_time')
-        shown = state['show'] and (pendulum.parse(toggle_time).add(seconds=25) > fs.now if toggle_time else False)
+
+        shown = state['show'] and in_future(state.get('toggle_time'), seconds=25)
         state['visible'] = idfm.is_active() or shown
 
         return state
