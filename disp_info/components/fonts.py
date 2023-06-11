9c168ec8f0b699b8e82c61b67bd0df57388bbb3d
Prashant Sinha <prashant@noop.pw>
2023-06-11 23:10:15 +0200

    add font attribution in source.

--- disp_info/components/fonts.py
+++ disp_info/components/fonts.py
@@ -1,25 +1,35 @@
 from PIL import ImageFont
 
-# All Paths are assumed to be from the package root.
+# All font paths are from the package root.
 
-# Tamzen
+# TAMZEN
+# > License   Free (non-standard)
+# > Source    https://github.com/sunaku/tamzen-font
 tamzen__rs = ImageFont.truetype('assets/fonts/TamzenForPowerline5x9r.ttf', 9)
 tamzen__rm = ImageFont.truetype('assets/fonts/Tamzen7x13r.ttf', 13)
 
-# Pixel Operator.
+# PIXEL OPERATOR
+# > License   CC0
+# > Source    https://notabug.org/HarvettFox96/ttf-pixeloperator
 px_op__r        = ImageFont.truetype('assets/fonts/PixelOperator8.ttf', 8)
 px_op_mono_8    = ImageFont.truetype('assets/fonts/PixelOperatorMono8.ttf', 8)
 px_op__l        = ImageFont.truetype('assets/fonts/PixelOperator.ttf', 16)
 px_op__xl       = ImageFont.truetype('assets/fonts/PixelOperator.ttf', 32)
 px_op__xxl      = ImageFont.truetype('assets/fonts/PixelOperator.ttf', 48)
 
-cd_icon = ImageFont.truetype('assets/fonts/CD-IconsPC.ttf', 22)
-
-# Scientifica
+# SCIENTIFICA
+# > License   OFL v1.1
+# > Source    https://github.com/nerdypepper/scientifica
 scientifica__r = ImageFont.truetype('assets/fonts/scientifica.ttf', 11)
 scientifica__b = ImageFont.truetype('assets/fonts/scientificaBold.ttf', 11)
 scientifica__i = ImageFont.truetype('assets/fonts/scientificaItalic.ttf', 11)
 
-# BitOcra
+# BITOCRA
+# > License   OFL v1.1
+# > Source    https://github.com/ninjaaron/bitocra
 bitocra = ImageFont.truetype('assets/fonts/bitocra7.bdf', 7)
+
+# COZETTE
+# > License   MIT
+# > Source    https://github.com/slavfox/Cozette
 cozette = ImageFont.truetype('assets/fonts/cozette.bdf', 13)
