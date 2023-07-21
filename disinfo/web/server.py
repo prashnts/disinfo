9f624ffcc26a65bf33fbc33f0f3a6fa74a616478
Prashant Sinha <prashant@noop.pw>
2023-07-21 23:30:08 +0200

    cleanup

--- disinfo/web/server.py
+++ disinfo/web/server.py
@@ -1,7 +1,3 @@
-import json
-import time
-import asyncio
-
 from fastapi import FastAPI, WebSocket
 from starlette.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
@@ -13,7 +9,6 @@ pubsub = db.pubsub(ignore_subscribe_messages=True)
 pubsub.subscribe('di.pubsub.frames')
 
 app = FastAPI()
-
 
 frame = None
 
@@ -28,39 +23,13 @@ PubSubManager().attach('frames', ('di.pubsub.frames',), load_frame)
 async def index() -> RedirectResponse:
     return RedirectResponse('/web/index.html')
 
-@app.websocket("/ws")
+@app.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
-    print(websocket.state)
-
-
     while True:
-        data = await websocket.receive_text()
+        await websocket.receive_text()
         if frame:
             await websocket.send_text(frame)
-        # time.sleep(0.1)
-        print('here')
-        continue
-        # message = pubsub.get_message()
-        # if not message:
-            # continue
-        print('here2')
-        try:
-            data = json.loads(message['data'].decode())
-            action = data['_action']
-            if action == 'new-frame':
-                await websocket.send_text(data['img'])
-        except KeyError:
-            pass
-
-    # try:
-    #     results = await subscribe.listen()
-    #     for result in results:
-    #         await websocket.send_text('test')
-    #         print('test send')
-    # except Exception as e:
-    #     await websocket.close()
-    #     raise e
 
 app.mount('/web', StaticFiles(directory='web'), name='web')
