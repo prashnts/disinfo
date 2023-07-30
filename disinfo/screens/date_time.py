d4047e39e81f3aaf64bb73e4fee1c94914ce3667
Prashant Sinha <prashant@noop.pw>
2023-07-30 22:31:29 +0200

    datetime date is in bitocra

--- disinfo/screens/date_time.py
+++ disinfo/screens/date_time.py
@@ -7,13 +7,13 @@ from ..components.layouts import hstack, vstack
 from ..components.text import TextStyle, text
 
 
-s_date      = TextStyle(color=gray.darken(.2).hex, font=fonts.tamzen__rs)
+s_date      = TextStyle(color=gray.darken(.2).hex, font=fonts.bitocra)
 s_hour      = TextStyle(color=gray.hex, font=fonts.px_op__l)
 s_minute    = TextStyle(color=gray.hex, font=fonts.px_op__l)
 s_seconds   = TextStyle(color=gray.darken(.2).hex, font=fonts.bitocra)
 s_day = {
-    'weekend': TextStyle(color=light_gray.darken(.1).hex, font=fonts.tamzen__rs),
-    'weekday': TextStyle(color=black.hex, font=fonts.tamzen__rs),
+    'weekend': TextStyle(color=light_gray.darken(.1).hex, font=fonts.bitocra),
+    'weekday': TextStyle(color=black.hex, font=fonts.bitocra),
 }
 s_day_box = {
     'weekend': DivStyle(radius=2, background=amber_red.darken(.1).hex, padding=[1, 0, 1, 1]),
