3203fa4ae0698337d27e60676b6705e85c965482
Prashant Sinha <prashant@noop.pw>
2023-08-20 20:33:02 +0200

    remote buttons in web

--- disinfo/web/server.py
+++ disinfo/web/server.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, WebSocket
+from typing import Annotated
+from fastapi import FastAPI, WebSocket, Body
 from starlette.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
 
 from disinfo.drat.app_states import PubSubManager, PubSubMessage
-from ..redis import db
+from disinfo.data_structures import AppBaseModel
+from ..redis import db, publish
 
 pubsub = db.pubsub(ignore_subscribe_messages=True)
 pubsub.subscribe('di.pubsub.frames')
@@ -22,6 +24,15 @@ PubSubManager().attach('frames', ('di.pubsub.frames',), load_frame)
 @app.get('/')
 async def index() -> RedirectResponse:
     return RedirectResponse('/web/index.html')
+
+class RemoteInput(AppBaseModel):
+    action: str
+
+@app.post('/remote')
+async def trigger_remote(rinput: RemoteInput):
+    publish('di.pubsub.remote', action=rinput.action)
+    print(rinput.action)
+    return {'status': 'ok'}
 
 @app.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket):
