44029036b00b5951f0190f9970fd1db7c0b02a58
Prashant Sinha <prashant@noop.pw>
2023-10-30 23:58:36 +0100

    trash

--- disinfo/components/elements.py
+++ disinfo/components/elements.py
@@ -1,11 +1,8 @@
-import numpy as np
-
 from PIL import Image
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 from typing import Optional, Union, Any
 
 
-Postion = tuple[int, int]
 _hash = hash
 
 class UIElement(metaclass=ABCMeta):
