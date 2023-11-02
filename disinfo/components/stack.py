afe646af9e0543e0639651583128e20008acd736
Prashant Sinha <prashant@noop.pw>
2023-11-02 20:10:55 +0100

    stack 4.2

--- disinfo/components/stack.py
+++ disinfo/components/stack.py
@@ -28,7 +28,7 @@ class Stack(metaclass=UniqInstance):
 
     def surface(self, fs: FrameState):
         frames = [w.draw(fs, active=i == self.pos) for i, w in enumerate(self._widgets)]
-        pos = app_config.height + sum([f.height for f in frames[0:self.pos] if f]) + (self.pos - 1 * 2)
+        pos = app_config.height - 8 + sum([f.height for f in frames[0:self.pos] if f]) + (self.pos - 1 * 2)
         return div(vstack(frames, gap=2), DivStyle(padding=(0, 0, 0, 2))), pos
 
     def tick(self, step: float):
