26a1f8918b59e85e3d46ebefe2e0517668d44959
Prashant Sinha <prashant@noop.pw>
2024-05-23 20:13:49 +0200

    auto dishwasher

--- disinfo/screens/dishwasher.py
+++ disinfo/screens/dishwasher.py
@@ -38,18 +38,18 @@ class DishwasherStateManager(PubSubStateManager[DishwasherState]):
 
 
 @cache
-def washer_lcd(letter):
+def washer_lcd(letter: str):
     mapping = {
-        1: ['b2', 'd2'],
-        2: ['a', 'b2', 'c', 'e', 'd1'],
-        3: ['a', 'b2', 'c', 'e', 'd2'],
-        4: ['b1', 'b2', 'c', 'd2'],
-        5: ['a', 'b1', 'c', 'd2', 'e'],
-        6: ['a', 'b1', 'c', 'd1', 'd2', 'e'],
-        7: ['a', 'b2', 'd2'],
-        8: ['a', 'b1', 'b2', 'c', 'd1', 'd2', 'e'],
-        9: ['a', 'b1', 'b2', 'c', 'd2', 'e'],
-        0: ['a', 'b1', 'b2', 'd1', 'd2', 'e'],
+        '1': ['b2', 'd2'],
+        '2': ['a', 'b2', 'c', 'e', 'd1'],
+        '3': ['a', 'b2', 'c', 'e', 'd2'],
+        '4': ['b1', 'b2', 'c', 'd2'],
+        '5': ['a', 'b1', 'c', 'd2', 'e'],
+        '6': ['a', 'b1', 'c', 'd1', 'd2', 'e'],
+        '7': ['a', 'b2', 'd2'],
+        '8': ['a', 'b1', 'b2', 'c', 'd1', 'd2', 'e'],
+        '9': ['a', 'b1', 'b2', 'c', 'd2', 'e'],
+        '0': ['a', 'b1', 'b2', 'd1', 'd2', 'e'],
         'h': ['b1', 'c', 'd1', 'd2'],
     }
     with open('assets/dishwasher-segment.svg', 'rb') as f:
