ff90788c00767cb6dba493764a0fed430240204a
Prashant Sinha <prashant@noop.pw>
2024-06-16 20:08:54 +0200

    implement adsbx

--- disinfo/screens/aviator/widgets.py
+++ disinfo/screens/aviator/widgets.py
@@ -7,6 +7,7 @@ from disinfo.components.text import TextStyle, text
 from disinfo.components.transitions import text_slide_in
 from disinfo.components import fonts
 from disinfo.utils.cairo import load_svg, load_svg_string
+from disinfo.screens.colors import gray
 
 from .state import ADSBxStateManager
 
@@ -20,10 +21,10 @@ def airplane_widget(fs: FrameState, plane: dict) -> Widget:
         vstack([
             text_slide_in(fs, f'avi.w.{plane["hex"]}.flight', plane.get('flight').strip(), TextStyle(font=fonts.px_op_mono_8, color='#106822')),
             hstack([
-                text_slide_in(fs, f'avi.w.{plane["hex"]}.alt', f"{plane.get('alt_baro'):0d}", TextStyle(font=fonts.cozette, color='#106822')),
-                text('m', TextStyle(font=fonts.bitocra7, color='#b27853')),
-            ]),
-        ]),
+                text_slide_in(fs, f'avi.w.{plane["hex"]}.alt', f"{plane.get('alt_baro'):0d}", TextStyle(font=fonts.bitocra7, color=gray.hex)),
+                text('m', TextStyle(font=fonts.bitocra7, color=gray.darken(0.1).hex)),
+            ], align='bottom', gap=1),
+        ], gap=2),
     ])
     return Widget(name='aviator.airplane_widget', frame=frame, priority=2, wait_time=10)
 
