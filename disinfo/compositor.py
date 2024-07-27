5196069d032a25a401dacd357212bf30d04cf49b
Prashant Sinha <prashant@noop.pw>
2024-07-27 23:54:01 +0200

    radar map

--- disinfo/compositor.py
+++ disinfo/compositor.py
@@ -77,19 +77,20 @@ def compose_small_frame(fs: FrameState):
         return Frame(image).tag('not_present')
 
     composite_at(screens.aviator.app.radar(fs), image, 'mm')
-    # composite_at(screens.solar.draw(fs), image, 'mm')
-    stack = Stack('main_cards').mut([
-        *screens.aviator.widgets.planes(fs),
-        screens.weather.widgets.weather(fs),
-        screens.dishwasher.widget(fs),
-        screens.weather.widgets.moon_phase(fs),
-        screens.now_playing.widget(fs),
-        screens.klipper.widget(fs),
-        screens.trash_pickup.widget(fs),
-        screens.date_time.calendar_widget(fs),
-    ])
-    composite_at(stack.draw(fs), image, 'ml')
     composite_at(screens.date_time.sticky_widget(fs), image, 'tr', dy=2)
+    # composite_at(screens.solar.draw(fs), image, 'mm')
+    # stack = Stack('main_cards').mut([
+    #     *screens.aviator.widgets.planes(fs),
+    #     screens.weather.widgets.weather(fs),
+    #     screens.dishwasher.widget(fs),
+    #     screens.weather.widgets.moon_phase(fs),
+    #     screens.now_playing.widget(fs),
+    #     screens.klipper.widget(fs),
+    #     screens.trash_pickup.widget(fs),
+    #     screens.date_time.calendar_widget(fs),
+    # ])
+    # composite_at(stack.draw(fs), image, 'ml')
+    # composite_at(screens.date_time.sticky_widget(fs), image, 'tr', dy=2)
     composite_at(screens.twenty_two.draw(fs), image, 'mm')
 
     return Frame(image).tag('present')
