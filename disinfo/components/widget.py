f5066c0ea2075e0281876bd105c07ace8af85c17
Prashant Sinha <prashant@noop.pw>
2024-11-17 12:23:19 +0100

    stack add

--- disinfo/components/widget.py
+++ disinfo/components/widget.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, replace as dc_replace
 from typing import Optional
 
 from disinfo.data_structures import FrameState
+from disinfo.utils import ease
 
 from .elements import Frame
 from .transitions import ScaleIn
@@ -17,10 +18,11 @@ class Widget:
     active: bool = True
     focus: bool = False
     wait_time: float = 2
+    transition_duration: float = 1
     style: DivStyle = DivStyle(padding=1, radius=2, background='#000000ca', border=1)
 
     def draw(self, fs: FrameState, active: bool = False) -> Optional[Frame]:
-        transition = ScaleIn(f'{self.name}.scalein', 2)
+        transition = ScaleIn(f'{self.name}.scalein', self.transition_duration, ease.cubic.cubic_in)
         if self.frame:
             style = dc_replace(self.style, border_color='#155598a9' if active else '#000000cf')
             return transition.mut(div(self.frame, style).tag(self.frame.hash)).draw(fs)
