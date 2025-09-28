571f5645f53f0535473ed1e982be8777475b6d91
Prashant Sinha <prashant@noop.pw>
2025-09-28 14:56:35 +0200

    client2

--- clients/websocket_rpi_matrix/main.py
+++ clients/websocket_rpi_matrix/main.py
@@ -5,5 +5,10 @@ from hub75_websocket_client import Config, main
 
 
 if __name__ == "__main__":
-    conf = Config()
+    try:
+        with open(os.environ.get('DI_WEBSOCKET_CLIENT_CONFIG', '.config.json')) as fp:
+            conf = Config(**json.load(fp))
+    except FileNotFoundError:
+        conf = Config()
+
     main(conf)
