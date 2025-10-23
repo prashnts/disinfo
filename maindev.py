dbbb33c8a94ef814e6a5e9ad647f634ea75126bd
Prashant Sinha <prashant@noop.pw>
2025-10-23 23:57:22 +0200

    cleanup + modulino

--- maindev.py
+++ maindev.py
@@ -5,22 +5,34 @@ import uvicorn
 from multiprocessing import Process
 
 from disinfo.renderers.background import main as background_renderer
+from disinfo.drat.app_states import PubSubManager, PubSubMessage
 from disinfo.drat.data_service import main as data_service_main
 from disinfo.drat.ha_service import main as ha_service_main
 from disinfo.redis import publish
 
+acts = None
 
+def passthru_acts(channel_name, message: PubSubMessage):
+    global acts
+    acts = [message.payload['cmd']]
+
+PubSubManager().attach('maindev', ('di.pubsub.acts',), passthru_acts)
 
 def run_sensors():
     os.environ['BLINKA_MCP2221'] = '1'
     from websocket_rpi_matrix.di_remote import setup as setup_sensors, sensor_loop
 
     def callback(payload):
+        global acts
         # print(f"[maindev] Sensor payload: {payload}")
         publish('di.pubsub.telemetry', action='update', payload={'data': json.dumps(payload)})
+        if acts:
+            local_acts = acts.copy()
+            acts = None
+            return local_acts
 
     print("[maindev] Setting up sensors")
-    sensors = setup_sensors()
+    sensors = setup_sensors(with_tof=False)
     print("[maindev] Starting sensor thread")
     sensor_loop(sensors, callback)
 
@@ -29,16 +41,21 @@ def run_server():
     uvicorn.run("disinfo.web.server:app", host="0.0.0.0", port=4215)
 
 
-
 if __name__ == "__main__":
-    procs = [background_renderer, data_service_main, ha_service_main, run_server, run_sensors]
+    procs = {
+        'webserver': run_server,
+        'renderer': background_renderer,
+        # 'data_loop': data_service_main,
+        'ha_loop': ha_service_main, 
+        'sensor_loop': run_sensors,
+    }
     processes = []
     try:
-        for proc in procs:
+        for name, proc in procs.items():
             p = Process(target=proc)
             p.start()
             processes.append(p)
-            print(f"[maindev] Started process {proc.__name__} with PID {p.pid}")
+            print(f"[maindev] Started process {name} with PID {p.pid}")
     finally:
         for p in processes:
             p.join()
